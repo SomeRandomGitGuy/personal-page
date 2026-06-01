ae8b1973ef21263f56507b08ba444c602b7cf0dc
Grant Fitzgibbon <grant.fitzgibbon@gmail.com>
2026-06-01 20:39:23 +1000

    hope

--- projects/flashcard-sheets/script.js
+++ projects/flashcard-sheets/script.js
@@ -80,7 +80,7 @@ async function getData(){
   let url = document.getElementById("url").value;
   // url = url.slice(0,url.indexOf("/edit?"))
   // url += "/export?format=tsv";
-  url = url.replace("edit?","/export?format=tsv&");
+  url = url.replace("edit?","export?format=tsv&");
   console.log(url);
   let response = await fetch(url);
   result = await response.text();
